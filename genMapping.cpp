a90c9accabb40c065b63632aeca63f136310085e
Mathevilg <109210548+Mathevilg@users.noreply.github.com>
2023-10-04 15:11:02 +0530

    Update genMapping.cpp

--- genMapping.cpp
+++ genMapping.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 
@@ -14,6 +15,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int vertices, edges;
+    std::string first_line;
+    std::getline(input_file1, first_line);
+    std::istringstream iss(first_line);
+    iss >> vertices >> edges;
+    std::cout << "vertices: " << vertices << std::endl;
+    std::cout << "edges: " << edges << std::endl;
+
     std::ifstream input_file2(argv[2]);
     if (!input_file2) {
         std::cerr << "Error: Could not open input file " << argv[2] << std::endl;
@@ -22,13 +31,28 @@ int main(int argc, char *argv[]) {
 
     // Process input file 1 and 2 (satoutput and graph) and generate the required output (stdout) in mapping file
     std::string line;
-    while (std::getline(input_file1, line)) {
-        // Process the line as needed
-        // ...
+    std::getline(input_file2, line);
+    std::istringstream iss1(line);
+    std::string s;
+    char minus = '-';
 
-        // Print the processed line to stdout
-        std::cout << line << std::endl;
+    std::cout << "#1\n";
+    for (int i=0; i<vertices ; i++) {
+        iss1 >> s;
+        if (s[0]!=minus) {
+            std::cout << s << " ";
+        }
     }
+    std::cout << "\n";
 
+    std::cout << "#2\n";
+    for (int i=0; i<vertices ; i++) {
+        iss1 >> s;
+        if (s[0]!=minus) {
+            std::cout << s << " ";
+        }
+    }
+    std::cout << "\n";
+    
     return 0;
 }
