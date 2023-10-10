d64b0387ef4eb8186b91a1104e13b3bdea7fb4c0
Mathevilg <109210548+Mathevilg@users.noreply.github.com>
2023-10-10 13:39:26 +0530

    Update part2.cpp

--- part2.cpp
+++ part2.cpp
@@ -51,89 +51,12 @@ int main(int argc, char *argv[]) {
     int k = 1;
     int constraint_count;
     std::string command = "minisat temp.txt result.txt";
-    while (k<=vertices) {
-        constraint_count = (vertices*(vertices-1))/2 - count + 4*vertices*k + vertices + k + 2;
-        std::ofstream outputFile("temp.txt", std::ios::trunc);
-        outputFile << "p cnf " + std::to_string(vertices+(vertices+1)*(k+1)) + " " + std::to_string(constraint_count) << std::endl;
-        for (int i = 0; i < vertices; i++) {
-            for (int j=i+1; j < vertices; j++) {
-                if (adjacencyMatrix[i][j]) {
-                    // Create a line to write and write the line to the file
-                    std::string line1 = "-" + std::to_string(i+1) + " -" + std::to_string(j+1) + " 0\n";
-                    outputFile << line1;
-                }            
-            }
-            
-        }
-
-        std::string line2;
-        std::string line3;
-        std::string line4;
-        std::string line5;
-        int a;
-        int b;
-        for (int i=0; i<=vertices; i++) {
-            for (int j=0; j<=k; j++){
-                if (j==0) {
-                    line2 = std::to_string(i*(k+1)+1+vertices) + " 0\n";
-                    outputFile << line2;
-                }
-                else if (i==0) {
-                    line2 = "-" + std::to_string(j+1+vertices) + " 0\n";
-                    outputFile << line2;
-                }
-                else {
-                    a = i*(k+1) + (j+1) + vertices;
-                    b = (i-1)*(k+1) + (j+1) + vertices;
-                    line2 = std::to_string(a) + " -" + std::to_string(b) + " 0\n";
-                    line3 = "-"+std::to_string(b-1) + " -" + std::to_string(i) + " " + std::to_string(a) + " 0\n";
-                    line4 = "-"+std::to_string(a) + " " + std::to_string(b) + " " + std::to_string(b-1) + " 0\n";
-                    line5 = "-"+std::to_string(a) + " " + std::to_string(b) + " " + std::to_string(i)+" 0\n";
-                    outputFile << line2 << line3 << line4 << line5;
-                }
-            }
-        }
-        outputFile << std::to_string(vertices+(vertices+1)*(k+1))+" 0\n";
-        std::cout << "k:" << std::to_string(k) << "\n";
-        k*=2;
-        
-        outputFile.close();
-
-        // Check if outputFile is satisfiable 
-        // if not satisfiable then break
-        int returnCode = system(command.c_str());
-        std::ifstream result_file("result.txt");
-        std::string result_line;
-        std::getline(result_file, result_line);
-        std::istringstream iss_result(result_line);
-        std::string sat;
-        iss_result >> sat;
-        if (sat == "UNSAT") break;
-        else {
-            std::string result_line2;
-            std::getline(result_file, result_line2);
-            std::istringstream iss_result2(result_line2);
-            std::string s;
-            
-            std::ofstream outputFile2(argv[2], std::ios::trunc);
-            outputFile2 << "#1\n";
-            for (int i=0; i<vertices ; i++) {
-                iss_result2 >> s;
-                if (s[0]!='-') {
-                    outputFile2 << s << " ";
-                }
-            }
-            std::cout << "\n";
-            outputFile2.close();
-        }
-
-    }
     // now find between k/2 and k-1 (both including)
-    int low = k/2;    // Lower bound
-    int high = k;    // Upper bound
+    int low = 1;    // Lower bound
+    int high = vertices;    // Upper bound
 
-    while (low+1 < high) {
-        int mid = low + (high - low) / 2;
+    while (low <= high) {
+        int mid = (high + low) / 2;
         k = mid;
         constraint_count = (vertices*(vertices-1))/2 - count + 4*vertices*k + vertices + k + 2;
         std::ofstream outputFile("temp.txt", std::ios::trunc);
@@ -177,7 +100,7 @@ int main(int argc, char *argv[]) {
             }
         }
         outputFile << std::to_string(vertices+(vertices+1)*(k+1))+" 0\n";
-        std::cout << "k:" << std::to_string(k) << "\n";
+        std::cout << "\n\n\n\n\nk:" << std::to_string(k) << "\n";
         k*=2;
         
         outputFile.close();
@@ -191,15 +114,14 @@ int main(int argc, char *argv[]) {
         std::istringstream iss_result(result_line);
         std::string sat;
         iss_result >> sat;
-        if (sat == "UNSAT") high = mid;
-        else low = mid;
-
+        if (sat == "UNSAT") high = mid-1;
+        else low = mid+1;
     }
 
 
 
 
-    k = low;
+    k = 16;
     constraint_count = (vertices*(vertices-1))/2 - count + 4*vertices*k + vertices + k + 2;
         std::ofstream outputFile("temp.txt", std::ios::trunc);
         outputFile << "p cnf " + std::to_string(vertices+(vertices+1)*(k+1)) + " " + std::to_string(constraint_count) << std::endl;
@@ -242,8 +164,7 @@ int main(int argc, char *argv[]) {
             }
         }
         outputFile << std::to_string(vertices+(vertices+1)*(k+1))+" 0\n";
-        std::cout << "k:" << std::to_string(k) << "\n";
-        k*=2;
+        std::cout << "\n\n\n\n\nk:" << std::to_string(k) << "\n";
         
         outputFile.close();
 
@@ -263,14 +184,17 @@ int main(int argc, char *argv[]) {
         std::string s;
         
         std::ofstream outputFile2(argv[2], std::ios::trunc);
-        outputFile2 << "#1\n";
-        for (int i=0; i<vertices ; i++) {
-            iss_result2 >> s;
-            if (s[0]!='-') {
-                outputFile2 << s << " ";
+        if (result_line=="UNSAT") outputFile2 << 0;
+        else {
+            outputFile2 << "#1\n";
+            for (int i=0; i<vertices ; i++) {
+                iss_result2 >> s;
+                if (s[0]!='-') {
+                    outputFile2 << s << " ";
+                }
             }
+            std::cout << "\n";
         }
-        std::cout << "\n";
         outputFile2.close();
         
 
